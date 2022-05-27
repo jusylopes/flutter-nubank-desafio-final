beb52d7f1eb36cbff2c6c759991b5780f46f3199
jusylopes <jusylopes@outlook.com>
2022-05-27 00:33:35 -0300

    implementacao de alert_dialog

--- lib/resources/las_colors.dart
+++ lib/resources/las_colors.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 
 abstract class LasColors {
   static const colorBackgroundColor = Colors.transparent;
-  
+
   // texto
-  static Color textTitleLoginPage = const Color(0XFF184F8D).withOpacity(0.8);
+  static Color textTitleInitialPage = const Color(0XFF184F8D).withOpacity(0.8);
   static const Color textLoginPage = Color(0XFF555770);
   static const Color textLoginPageCreate = Color(0XFF2A81DD);
   static const Color textRegisterPage = Color(0XFF000000);
   static const Color txtLoginRegisterPageSubtitle = Color(0XFF555770);
+  static const Color txtAlertWidget = Color(0XFF555770);
 
   // botao
   static const Color buttonColor = Color(0XFF004BA0);
+  static const Color buttonColorAwait = Color(0XFF828282);
   static const Color textButtonColor = Color(0XFFFFFFFF);
+  
 }
