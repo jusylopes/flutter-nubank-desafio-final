3fa33af78f11b4be39bb83fa2809ea8a481ae2cf
isaiaspires <95889740+isaiaspires@users.noreply.github.com>
2022-05-27 16:14:48 -0300

    form

--- lib/data/entity/credentials_entity.dart
+++ lib/data/entity/credentials_entity.dart
@@ -2,5 +2,8 @@ class CredentialsEntity {
   final String username;
   final String password;
 
-  CredentialsEntity({required this.username, required this.password});
+  CredentialsEntity({
+    required this.username,
+    required this.password,
+  });
 }
