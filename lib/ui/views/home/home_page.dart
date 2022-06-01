3cbac0f99f080f01e0653e16cfb4cbaf4e15437c
isaiaspires <95889740+isaiaspires@users.noreply.github.com>
2022-05-31 23:15:50 -0300

    Correcao Registro

--- lib/ui/views/home/home_page.dart
+++ lib/ui/views/home/home_page.dart
@@ -137,10 +137,4 @@ class HomePage extends StatelessWidget {
       ],
     );
   }
-
-  // Future<bool> logout() async {
-  //   SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-  //   await sharedPreferences.clear();
-  //   return true;
-  // }
 }
