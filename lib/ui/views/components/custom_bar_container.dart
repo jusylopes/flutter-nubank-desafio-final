e612d04c6e9ba6f402a2f38f256c32d76462c155
Daiane Santos <daiane.ssantos01@gmail.com>
2022-06-03 22:43:58 -0300

    ajustesAppBarHistoricPage

--- lib/ui/views/components/custom_bar_container.dart
+++ lib/ui/views/components/custom_bar_container.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../../resources/las_colors.dart';
 import 'custom_appbar.dart';
 
-// Para ampliar a imagem de fundo do background cobrindo ate o appbar
+// Para ampliar a imagem de fundo do background cobrindo ate o appbar na tela de historico
 class CustomBarContainer extends StatelessWidget {
   final String titleappbarhistoric;
 
@@ -17,20 +18,20 @@ class CustomBarContainer extends StatelessWidget {
         alignment: Alignment.center,
         height: 160,
         width: MediaQuery.of(context).size.width,
-        color: const Color.fromARGB(255, 1, 117, 180),
+        color: LasColors.appBarEditProfile,
         child: Row(
           children: [
             IconButton(
                 onPressed: () {},
                 icon: const Icon(
                   Icons.arrow_back_ios_new,
-                  color: Colors.white,
+                  color: LasColors.txtAppBarProfile,
                 )),
             const Spacer(),
             Text(
               titleappbarhistoric,
               style: const TextStyle(
-                  color: Colors.white,
+                  color: LasColors.txtAppBarProfile,
                   fontSize: 20,
                   fontWeight: FontWeight.w500),
             ),
