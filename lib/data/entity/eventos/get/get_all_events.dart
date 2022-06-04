e99303d5c4a7a8529ab7a5eb5258d346d5a4c8c5
isaiaspires <95889740+isaiaspires@users.noreply.github.com>
2022-06-04 12:44:19 -0300

    All Events Atualizado

--- lib/data/entity/eventos/get/get_all_events.dart
+++ lib/data/entity/eventos/get/get_all_events.dart
@@ -16,26 +16,4 @@ class GetAllEvents {
     required this.endDate,
     required this.status,
   });
-
-  // GetAllEvents.fromJson(Map<String, dynamic> json) {
-  //   id = json['id'];
-  //   name = json['name'];
-  //   description = json['description'];
-  //   imageUrl = json['imageUrl'];
-  //   startDate = json['startDate'];
-  //   endDate = json['endDate'];
-  //   status = json['status'];
-  // }
-
-  // Map<dynamic, dynamic> toJson() {
-  //   final Map<dynamic, dynamic> data = <String, dynamic>{};
-  //   data['id'] = id;
-  //   data['name'] = name;
-  //   data['description'] = description;
-  //   data['imageUrl'] = imageUrl;
-  //   data['startDate'] = startDate;
-  //   data['endDate'] = endDate;
-  //   data['status'] = status;
-  //   return data;
-  // }
 }
